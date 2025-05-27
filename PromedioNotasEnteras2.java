be642e2ab263a52ec4366b1adff63366b7c6f9d3
Enrique-guajardo <114340323+Enrique-guajardo@users.noreply.github.com>
2025-05-26 21:26:28 -0400

    Update PromedioNotasEnteras2.java
    Código aun mas limpio

--- PromedioNotasEnteras2.java
+++ PromedioNotasEnteras2.java
@@ -2,51 +2,42 @@ import java.util.Scanner;
 
 public class PromedioNotasEnteras {
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
     
-        String notaTexto1, notaTexto2, notaTexto3; 
-        int nota1, nota2, nota3;
-        int promedio;
+    String notaTexto1, notaTexto2, notaTexto3;
+    int nota1, nota2, nota3;
+    int promedio;
 
-        
-        Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
-        
-        System.out.println("--- Calculador de Promedio de Notas ---");
+    System.out.println("Promediador de Notas (1 al 7)");
 
-        // Nota 1
-        System.out.print("Ingresa la primera nota (entre 1 y 7): ");
-        notaTexto1 = sc.nextLine();
+    System.out.println("Ingresa la primera nota: ");
+    notaTexto1 = sc.nextLine();
 
-        // Nota 2
-        System.out.print("Ingresa la segunda nota (entre 1 y 7): ");
-        notaTexto2 = sc.nextLine(); 
+    System.out.println("Ingresa la segunda nota: ");
+    notaTexto2 = sc.nextLine();
 
-        // Nota 3
-        System.out.print("Ingresa la tercera nota (entre 1 y 7): ");
-        notaTexto3 = sc.nextLine(); 
+    System.out.println("Ingresa la tercera nota: ");
+    notaTexto3 = sc.nextLine();
 
-        
-        
-        
-        nota1 = Integer.parseInt(notaTexto1);
-        nota2 = Integer.parseInt(notaTexto2);
-        nota3 = Integer.parseInt(notaTexto3);
+    nota1 = Integer.parseInt(notaTexto1);
+    nota2 = Integer.parseInt(notaTexto2);
+    nota3 = Integer.parseInt(notaTexto3);
 
-        
-        promedio = (nota1 + nota2 + nota3) / 3;
+    promedio = (nota1 + nota2 + nota3) /3;
 
-        
-        
-        System.out.println("Tu promedio es: " + promedio);
+    System.out.println("Tu promedio es: "+ promedio);
 
-        if (promedio >= 4) {
-            System.out.println("¡Felicitaciones! Has APROBADO la asignatura.");
-        } else {
-            System.out.println("Lo sentimos, has REPROBADO la asignatura.");
-        }
 
-        
-        sc.close();
+    if (promedio >= 4){
+        System.out.println("Aprobado");
+    }else{
+        System.out.println("Reprobado");
     }
+ 
+    sc.close();
+
+    }
+    
 }
